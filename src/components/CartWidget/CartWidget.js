deb2875760323de6af52de139bfa1a0dee6ebbd9
Lucas Monti <lucassmonti@gmail.com>
2024-10-12 21:08:37 -0300

    segundaentregaparcial

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,12 @@
+import cart from "./assets/carritocompras.jpg"
+
+const CartWidget = () => {
+    return (
+        <div>
+            <img src={cart} alt="carritodecompras" />
+            0
+        </div>
+    )
+}
+
+export default CartWidget
