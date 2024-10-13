eec11abf4ed1ff6571d2db778e1df26d509867f6
Lucas Monti <lucassmonti@gmail.com>
2024-10-13 00:06:24 -0300

    otro avance

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemCount from './components/ItemCount/ItemCount';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 
